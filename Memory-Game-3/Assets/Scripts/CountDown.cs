1fc9c6b154a19809ce94d8bf529da71cf44356b5
nishiumi <xiyeliang737@gmail.com>
2024-04-24 14:58:48 +0900

    ゲームオーバーとリトライ追加

--- Memory-Game-3/Assets/Scripts/CountDown.cs
+++ Memory-Game-3/Assets/Scripts/CountDown.cs
@@ -7,13 +7,18 @@ using UnityEngine.UI;
 public class CountDown : MonoBehaviour
 {
     
-    public static float CountDownTime;    // カウントダウンタイム
-    
-    public Text TextCountDown;              // 表示用テキストUI
+    public static float CountDownTime;      // カウントダウンタイム
+    public GameObject GameOverText;         //ゲームオーバーUI
+    public GameObject exchangeButton;       //リトライボタン
+    public Text TextCountDown;              // カウントダウンの表示用テキストUI
 
     void Start()
     {
-        CountDownTime = 60.0f;    // カウントダウン開始秒数をセット
+        //最初は表示しない
+        GameOverText.SetActive(false);
+        exchangeButton.SetActive(false);
+
+        CountDownTime = 15.0f;              // カウントダウン開始秒数をセット　現在はデバック観点から１５秒
     }
 
     void Update()
@@ -22,15 +27,18 @@ public class CountDown : MonoBehaviour
         TextCountDown.text = String.Format("Time: {0:00.00}", CountDownTime);
         CountDownTime -= Time.deltaTime;
 
+        //１０秒切ったら色を赤に変える
         if (CountDownTime < 10.0f)
         {
-            TextCountDown.color= Color.red;
+            TextCountDown.color = Color.red;
         }
-        // 0.0秒以下になったらカウントダウンタイムを0.0で固定する
+        // 0.0秒以下になったらカウントダウンタイムを0.0で固定、UI系のの表示
         if (CountDownTime <= 0.0f)
         {
             CountDownTime = 0.0f;
+            GameOverText.SetActive(true);
+            exchangeButton.SetActive(true);
         }
 
-    }
+    }   
 }
